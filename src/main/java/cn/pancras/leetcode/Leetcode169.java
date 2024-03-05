f8fea0250328a71e475a241e2c7ec6545c92536b
PancrasDuan <dpqkindle@163.com>
2024-03-06 01:16:44 +0800

    filter .idea

---
+++ src/main/java/cn/pancras/leetcode/Leetcode169.java
@@ -0,0 +1,27 @@
+package cn.pancras.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author： Duanpengqi
+ * @date： 2024/3/6
+ * @description：
+ * @modifiedBy：
+ * @version: 1.0
+ */
+public class Leetcode169 {
+    
+    private Solution solution = new Solution();
+    
+    
+    public int solution(int[] nums) {
+        return solution.majorityElement(nums);
+    }
+    
+    
+    private class Solution {
+        public int majorityElement(int[] nums) {
+            return 0;
+        }
+    }
+}
