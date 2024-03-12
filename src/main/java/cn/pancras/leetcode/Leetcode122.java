7cf67e9a14cdcd6a3f3c300c63a5f0f3b4500c48
PancrasDuan <dpqkindle@163.com>
2024-03-12 23:36:09 +0800

    leetcode 122 by dp

--- src/main/java/cn/pancras/leetcode/Leetcode122.java
+++ src/main/java/cn/pancras/leetcode/Leetcode122.java
@@ -26,9 +26,33 @@ public class Leetcode122 {
         return totalProfit;
     }
     
+    /**
+     * dp 思路
+     * dp[i][0] 第i天持有股票的最大利润
+     * 1. 第i-1天持有股票的最大利润    dp[i-1][0]
+     * 2. 第i天买入股票的最大利润      dp[i-1][1] - prices[i]
+     * final : max(dp[i-1][0], dp[i-1][1] - prices[i])
+     *
+     * dp[i][1] 第i天不持有股票的最大利润
+     * 1. 第i-1天不持有股票最大利润    dp[i-1][1]
+     * 2. 第i天卖出股票的最大利润      dp[i-1][0] + prices[i]
+     * final : max(dp[i-1][1], dp[i-1][0] + prices[i])
+     */
     private class Solution {
         public int maxProfit(int[] prices) {
-            return 0;
+            if (prices.length == 0) {
+                return 0;
+            }
+            
+            int[][] dp = new int[prices.length][2];
+            dp[0][0] = 0 - prices[0];
+            dp[0][1] = 0;
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+                dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
+            }
+            
+            return dp[prices.length-1][1];
         }
     }
 }
